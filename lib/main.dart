42564b960d245806f113117fefd9bad68d76a964
Anass Arrahi Arrai <82566327+Anass-Arrahi-Arrai@users.noreply.github.com>
2024-06-04 11:51:43 +0200

    minor changes

--- lib/main.dart
+++ lib/main.dart
@@ -49,6 +49,7 @@ class MyApp extends StatelessWidget {
   const MyApp({super.key});
   @override
   Widget build(BuildContext context) {
+    clearAll();
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: HomePage(),
